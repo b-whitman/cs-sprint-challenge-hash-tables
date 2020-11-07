f3f98aa97e93589189d421b2e9a4166c1333dc8d
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-11-06 22:35:06 -0500

    completed ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,26 @@
 def has_negatives(a):
     """
-    YOUR CODE HERE
+    Return integers with corresponding negative values in list
     """
-    # Your code here
+    # Iterate through list
+    # If number is positive and not already in list, make it a key with value 0
+    # If positive and already in list, make value 1
+    # If number is negative, multiply it by -1 and check to see if that value is in dict
+    # If already keyed in, make value 1
+
+    has_neg = {}
+    for i in a:
+        if abs(i) not in has_neg:
+            has_neg[abs(i)] = 0
+        elif abs(i) in has_neg:
+            has_neg[abs(i)] = 1
+    
+    result = []
+    for i in list(has_neg):
+        if has_neg[i] == 1:
+            result.append(i)
+
+
 
     return result
 
